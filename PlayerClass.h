a76687672a352f4369c376fbd35fa6959e19b22d
Joseph Cochran <nwcochran11@gmail.com>
2018-03-06 18:26:15 -0500

    Validation/New header file
    Created namespace and updated header files

--- PlayerClass.h
+++ PlayerClass.h
@@ -2,6 +2,8 @@
 #define PLAYERCLASS_H_INCLUDED
 
 #include <iostream>
+#include <limits>
+#include "Weapon_Class.h"
 using std::cout;
 using std::string;
 using std::endl;
@@ -10,11 +12,13 @@ using std::cin;
 class Player
 {
     private:
+        Weapon weapon;
         int health;
         int exp_To_lvl = 100;
         int current_exp = 0;
         int strength, speed, magic, thirst, level;
         string name;
+
     public:
         int xLocation, yLocation;
         Player();
@@ -33,6 +37,7 @@ class Player
         void setHealth(int deff);
         string getName();
         void displayLevel( int str, int spd, int def, int mag, int lvl);
+        void setWeapon(Weapon &);
 };
 //Default struct for Player Class
 Player::Player()
@@ -70,14 +75,14 @@ void Player::setLevel(int levelll)
 {
     level = levelll;
 }
-
+//Level
 void Player::LevelUP()
 {
     Player::setLevel(++level);
     std::cout << Player::getName() << " is now level " << Player::getLevel() << "! " << endl;
     Player::displayLevel(Player::getStrength(),Player::getSpeed(),Player::getHealth(),Player::getMagic(),Player::getLevel());
  }
-
+//Getters and Setters
 int Player::getHealth()
 {
     return health;
@@ -125,35 +130,59 @@ void Player::setMagic(int magg)
 
 void Player::displayLevel( int str, int spd, int def, int mag, int lvl)
 {
+    bool repeat = false;
+    int counter = 0;
+    //variable for player to choose which stat to level up
     int statup;
+    //Prints current stats
     cout << "Awesome Adventurer: " << Player::getName() << endl << "Level: " << lvl << endl
         << "Current Stats: " << endl << "Strength: " << str << endl << "Speed: "
-        << spd << endl << "Defense: " << def << endl << "Magic: " << mag << endl;
+        << spd << endl << "Health: " << def << endl << "Magic: " << mag << endl;
 
     cout << endl << "--You have 3 points to put in your stats." << endl <<  "--Choose wisely, there are no take backs muahahahahahaha *cough cough*." << endl
-        << "--Press 1 for point in strength, 2 for speed, 3 for defense, and 4 for magic." << endl <<
+        << "--Press 1 for point in strength, 2 for speed, 3 for Health, and 4 for magic(The only magic in this land is magic obtained from the sacred scrolls of Achimel, so if you don't have any, we'd recommend not upgrading this stat." << endl <<
             "--Pretty simple, especially for the likes of you." << endl;
-
+    //Loop through to ask and assign stats to new stats
     for (int i = 0; i < 3; i++)
     {
-        cout << "Please choose stat to level up. " << endl;
-        cin >> statup;
-        switch (statup)
+        while (repeat == false)
         {
-            case 1: ++str;
-                    cout << "You have leveled up strength." << endl;
-                    break;
-            case 2: ++spd;
-                    cout << "You have leveled up speed." << endl;
-                    break;
-            case 3: ++def;
-                    cout << "You have leveled up defense." << endl;
-                    break;
-            case 4: ++mag;
-                    cout << "You have leveled up magic." << endl;
-                    break;
+            cout << "Please choose stat to level up. " << endl;
+            cin >> statup;
+        if (cin.fail() || statup > 4 || statup < 1)
+        {
+            repeat = false;
+            cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << endl << "Come on, just pick a valid stat. " << endl;
         }
+        else
+        {
+
+            switch (statup)
+            {
+                case 1: ++str;
+                        cout << "You have leveled up strength." << endl;
+                        break;
+                case 2: ++spd;
+                        cout << "You have leveled up speed." << endl;
+                        break;
+                case 3: ++def;
+                        cout << "You have leveled up Health." << endl;
+                        break;
+                case 4: ++mag;
+                        cout << "You have leveled up magic." << endl;
+                        break;
+            }
+            ++counter;
+            if (counter == 3)
+            {
+                break;
+            }
+        }
+
     }
+        //set everything to new stats
         Player::setHealth(def);
         Player::setMagic(mag);
         Player::setStrength(str);
@@ -161,10 +190,17 @@ void Player::displayLevel( int str, int spd, int def, int mag, int lvl)
 
         cout << "Your new stats: " << endl << "Strength: " << str << endl << "Speed: "
         << spd << endl << "Health: " << def << endl << "Magic: " << mag << endl;
+        break;
 
         cout << "Press literally anything to continue your adventure. Just not the power button." << endl;
         cin.ignore();
         cin.get();
+}
+}
+//Member function to set weapon of the player
+void Player::setWeapon(Weapon & weap)
+{
+    weapon = weap;
 }
 
 #endif // PLAYERCLASS_H_INCLUDED
--- Weapon_Class.h
+++ Weapon_Class.h
@@ -1,10 +1,67 @@
 #ifndef WEAPON_CLASS_H_INCLUDED
 #define WEAPON_CLASS_H_INCLUDED
+#include "Status_Namespace.h"
 class Weapon
 {
 private:
-
+    bool sword;
+    bool axe;
+    int dmg;
+    char type;
+public:
+    Weapon();
+    Weapon(bool , bool, int, char);
+    int set_damage_Output(Weapon &, char);
+    bool getSword();
+    bool getAxe();
+    int getDmg();
+    void setDmg(int);
 };
+
+//Getter method to get damage int value from class Weapon
+int Weapon::getDmg()
+{
+    return dmg;
+}
+//Setter method to set damage int value from class Weapon
+void Weapon::setDmg(int oldDmg)
+{
+    dmg = oldDmg;
+}
+//Getter method to get sword bool value from class Weapon
+bool Weapon::getSword()
+{
+    return sword;
+}
+//Getter method to get axe bool value from class Weapon
+bool Weapon::getAxe()
+{
+    return axe;
+}
+void Weapon::set_damage_Output(Weapon & weapon, char typeWpn)//MAY NEED TO INCLUDE MONSTER OBJECT TO CHECK RESISTANCES
+{
+    int damage = weapon.getDmg();
+    if (typeWpn == 'p')
+    {
+        //run future damage resistance function
+        damage = damage + status::poison; //Later make each status damage a global constant
+        return damage;
+    }
+    else if(typeWpn == 'b')
+    {
+        //run future damage resistance function
+        damage = damage + status::burn;
+        return damage;
+    }
+    else if(typeWpn == 'r')
+    {
+        //run future damage resistance function
+        damage = damage + status::bleed;
+        return damage;
+    }
+}
+
+
 
 
 #endif // WEAPON_CLASS_H_INCLUDED
