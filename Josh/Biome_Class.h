5e5175f51c46ea39f14d62abf5a7ee5e53f65a7c
Joshua Foss <jmfoss98@gmail.com>
2018-04-17 15:05:11 -0400

    Adding movement test

---
+++ Josh/Biome_Class.h
@@ -0,0 +1,34 @@
+#ifndef BIOME_CLASS
+#define BIOME_CLASS
+
+class Biome
+{
+    private:
+        int heat;
+        char biome;
+        struct Entities
+        {
+            char type = '\'';
+            int number = 0;
+            Entities() : type('\''), number(0)
+            {
+            }
+            Entities(char i, int j) : type(i), number(j)
+            {
+            }
+        };
+
+    public:
+        static int caveCount;
+        Biome();
+        Biome(int, char);
+        char map[MAPSIZEY][MAPSIZEX];
+        Entities entityMap[MAPSIZEY][MAPSIZEX];
+        vector<Cave> caves;
+        char getEntity(int, int);
+        void entityMapPop(int);
+
+
+};
+
+#endif // BIOME_CLASS
