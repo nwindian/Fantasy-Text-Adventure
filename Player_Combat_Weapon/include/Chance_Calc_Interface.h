227109464f2b97cfea614825a5b4b0e4a0a02bbf
Joseph Cochran <nwcochran11@gmail.com>
2018-05-24 16:00:20 -0400

    Added to Combat

--- Player_Combat_Weapon/include/Chance_Calc_Interface.h
+++ Player_Combat_Weapon/include/Chance_Calc_Interface.h
@@ -1,5 +1,5 @@
-#ifndef CHANCE_CALC_INTERFACE.H
-#define CHANCE_CALC_INTERFACE.H
+#ifndef CHANCE_CALC_INTERFACE
+#define CHANCE_CALC_INTERFACE
 
 #include "Moves_Class.h"
 bool chance(Player_Class &, Moves_Class &);
--- Player_Combat_Weapon/main.cpp
+++ Player_Combat_Weapon/main.cpp
@@ -9,13 +9,20 @@ using namespace std;
 
 int main()
 {
+    rlutil::setColor(2);
+    rlutil::saveDefaultColor();
     Moves_Class move("Awesome move", 5,5);
+    Moves_Class moves("Awesome moves", 5,5);
+    Moves_Class movess("Awesome movess", 5,5);
     Weapon_Class sword(5, 'd', "sword");
     Player_Class enemy("fish", 2, 2, 3, 0,0,2);
     Player_Class player("Joe",5,5,5,5,5,5);
-    player.LevelUP(player);
-    cin.get();
+//    player.LevelUP(player);
+//    rlutil::cls();
+    //cin.get();
     player.moveList.push_back(move);
+    player.moveList.push_back(moves);
+    player.moveList.push_back(movess);
     player.weaponInventory.push_back(sword);
     int a = playerBP(player, enemy);
    // player.LevelUP(player);
