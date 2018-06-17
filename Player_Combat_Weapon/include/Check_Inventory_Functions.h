8e281894c7624b6b6a0c6179bf0622c4c8b77294
Joseph Cochran <nwcochran11@gmail.com>
2018-06-16 23:34:49 -0400

    Just need to tweak inventory now

--- Player_Combat_Weapon/include/Check_Inventory_Functions.h
+++ Player_Combat_Weapon/include/Check_Inventory_Functions.h
@@ -1,8 +1,10 @@
 #ifndef CHECK_INVENTORY_FUNCTIONS_H_INCLUDED
 #define CHECK_INVENTORY_FUNCTIONS_H_INCLUDED
 #include "Player_Class.h"
+
 int getInventory();
-int getFromInventory(Player_Class&);
+int getFromInventoryF(Player_Class&);
+int getFromInventoryW(Player_Class&);
 
 
 #endif // CHECK_INVENTORY_FUNCTIONS_H_INCLUDED
