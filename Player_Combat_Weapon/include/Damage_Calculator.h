3240f4332a0dadac71ab3cf98f21f14a8ade884d
Joseph Cochran <nwcochran11@gmail.com>
2018-06-17 20:56:37 -0400

    Clean

--- Player_Combat_Weapon/include/Damage_Calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef DAMAGE_CALCULATOR_H_INCLUDED
-#define DAMAGE_CALCULATOR_H_INCLUDED
-#include "Monster.hpp"
-
-int dmg_calc(Player_Class &, Monster & , Weapon_Class & , Moves_Class &);
-int dmg_calc(Player_Class &, Monster &);
-
-
-#endif // DAMAGE_CALCULATOR_H_INCLUDED
