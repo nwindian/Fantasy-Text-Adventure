5acb37e336958455b794b1e7278afdfe3ef6e5f7
Joseph Cochran <nwcochran11@gmail.com>
2018-08-12 15:06:12 -0400

    moved map of types

--- player/player.h
+++ player/player.h
@@ -2,9 +2,11 @@
 #define PLAYER
 #include "../Player_Combat_Weapon/include/Player_Class.h"
 #include <vector>
+#include <map>
 namespace player
 {
     extern Player_Class players;
+    extern std::map<std::string, double> types;
 }
 
 
