95df00c6368070c694e92d5963f47fb397041659
Joseph Cochran <nwcochran11@gmail.com>
2018-09-01 18:23:59 -0400

    More to Equipment

---
+++ Player_Combat_Weapon/include/Get_Random_Equipment.h
@@ -0,0 +1,11 @@
+#ifndef GET_RANDOM_EQUIPMENT_H_INCLUDED
+#define GET_RANDOM_EQUIPMENT_H_INCLUDED
+#include "Weapon_Class.h";
+#include "Armor_Class.h"
+#include "Player_Class.h"
+
+Weapon_Class & getRandomWeapon(Player_Class &);
+Armor_Class & getRandomArmor(Player_Class &);
+
+
+#endif // GET_RANDOM_EQUIPMENT_H_INCLUDED
---
+++ Player_Combat_Weapon/src/Get_Random_Equipment.cpp
@@ -0,0 +1,6 @@
+//#include "../../include/Get_Random_Equipment.h"
+
+//Weapon_Class getRandomWeapon(Player_Class)
+//{
+
+//}
